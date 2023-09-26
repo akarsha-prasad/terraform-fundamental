f29964a2c1f3552e021b8b3c5f26214cf7486a77
Akarsha <86788384+akarsha-prasad@users.noreply.github.com>
2023-09-26 13:28:04 +0530

    Create ec2_vpc.tf

---
+++ ec2_vpc.tf
@@ -0,0 +1,49 @@
+resource "aws_key_pair" "mykey" {
+           key_name = "terra-key"
+           public_key = file("/home/ubuntu/.ssh/terra-key.pub")
+
+}
+
+resource "aws_default_vpc" "default_vpc" {
+
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+         name = "allow_ssh"
+         description = "Allow ssh inbound traffic"
+
+  # using default vpc
+
+          vpc_id = aws_default_vpc.default_vpc.id
+          ingress {
+              description = "TLS from VPC"
+
+  # we should allow incoming and outgoing
+  # TCP packets
+
+               from_port = 22
+               to_port  = 22
+               protocol = "tcp"
+
+  # allow all traffic
+
+               cidr_blocks = ["0.0.0.0/0"]
+}
+
+   tags = {
+     name = "allow_ssh"
+ }
+
+}
+
+resource "aws_instance" "my-vpc-instance" {
+             key_name = aws_key_pair.mykey.key_name
+             ami = var.ec2-ubuntu-ami
+             instance_type = "t2.micro"
+             security_groups = [aws_security_group.allow_ssh.name]
+
+             tags = {
+                     name = "secured-instance"
+}
+}
